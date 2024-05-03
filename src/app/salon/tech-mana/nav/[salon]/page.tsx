d5b9f495c8999d0843577c2959ce4efe0974fc7c
xXAnoHitoXx-lap <Anohito.turtled@gmail.com>
2024-05-02 22:36:11 -0300

    fixes cache invalidation and tech creation layout

--- src/app/salon/tech-mana/nav/[salon]/page.tsx
+++ src/app/salon/tech-mana/nav/[salon]/page.tsx
@@ -1,7 +1,7 @@
 export default async function Nav({ params }: { params: { salon: string } }) {
     return(
         <div className="flex flex-wrap w-full h-fit p-4 gap-2 justify-center">
-            <a href={ "/salon/tech-mana/new/r/".concat(params.salon)}>
+            <a href={ "/salon/tech-mana/new/0/".concat(params.salon)}>
                 <button className="border-2 border-sky-400 rounded-full w-32 h-20">New Technician</button>
             </a>
             <button className="border-2 border-sky-400 rounded-full w-32 h-20">Gift Card Manager</button>
