779e7710b7122b94d1ac2515dd77daf3ab062761
xXAnoHitoXx-lap <Anohito.turtled@gmail.com>
2024-05-07 14:51:10 -0300

    refactored

--- src/server/queries/business/location.ts
+++ src/server/queries/business/location.ts
@@ -3,7 +3,6 @@ import { f_db } from "~/server/db_schema";
 import { fb_location_entries, type Location } from "~/server/db_schema/fb_schema";
 
 export async function get_location_list(redirect = ""): Promise<Location[]> {
-
     const data: DataSnapshot = await get(ref(f_db, fb_location_entries(redirect)));
     const locations: Location[] = [];
 
