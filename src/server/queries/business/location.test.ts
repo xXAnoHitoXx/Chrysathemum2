9be80d7e9c7eb20e1979a2111b026a4a93a6d877
xXAnoHitoXx-lap <Anohito.turtled@gmail.com>
2024-05-07 18:54:53 -0300

    assigning technicians to location query

--- src/server/queries/business/location.test.ts
+++ src/server/queries/business/location.test.ts
@@ -1,6 +1,10 @@
-import { type Location, clear_test_data } from "~/server/db_schema/fb_schema";
+import { type Location, clear_test_data, type Technician } from "~/server/db_schema/fb_schema";
 import { create_new_location } from "../crud/location/location";
-import { get_location_list } from "./location";
+import { assign_technician_to_location, get_location_list, retrieve_technicians_at } from "./location";
+import { retrieve_roster_ids } from "../crud/location/location_roster";
+import { izip } from "itertools"
+import { create_new_technician, mark_technician_inactive } from "./technician_queries";
+import { retrieve_technician_entry } from "../crud/technician/technician_entry";
 
 const test_suit = "location_business_queries";
 
@@ -30,4 +34,52 @@ test("location list", async () => {
     const ids: string[] = list.map((location) => (location.id));
     expect(ids).toContain(salon1.id);
     expect(ids).toContain(salon2.id);
+})
+
+test("technician assignment", async () => {
+    const test_name = test_suit.concat("/technician_assignment/");
+
+    const salon = {
+        id: "5CBL",
+        address: "5 Cumberland"
+    }
+
+    const tech1_info = {
+        name: "LOLOLOL",
+        color: "aoisetnaoiesrt",
+    }
+
+    const tech2_info = {
+        name: "LOLlastonaOLOL",
+        color: "aoisetnaarsetnloiesrt",
+    }
+
+    await create_new_location(salon, test_name);
+    const tech1 = await create_new_technician(tech1_info, test_name);
+    const tech2 = await create_new_technician(tech2_info, test_name);
+    await mark_technician_inactive(tech2, test_name);
+
+    expect(tech1.active).toBe(true);
+    expect(tech2.active).toBe(false);
+
+    await assign_technician_to_location({location_id: salon.id, technician: tech1}, test_name);
+    await assign_technician_to_location({location_id: salon.id, technician: tech2}, test_name);
+
+    expect((await retrieve_technician_entry(tech2.id, test_name))?.active).toBe(true);
+
+    expect(tech1.active).toBe(true);
+    expect(tech2.active).toBe(true);
+    
+    const roster: string[] = await retrieve_roster_ids(salon.id, test_name);
+    const techs: Technician[] = await retrieve_technicians_at(salon.id, test_name);
+
+    expect(roster).toHaveLength(2);
+    expect(techs).toHaveLength(2);
+    expect(roster).toContain(tech1.id);
+    expect(roster).toContain(tech2.id);
+
+    expect(roster.length).toBe(techs.length);
+    for (const [roster_id, tech] of izip(roster,techs)) {
+        expect(tech.id).toBe(roster_id);
+    }
 })
--- src/server/queries/business/location.ts
+++ src/server/queries/business/location.ts
@@ -1,6 +1,8 @@
-import { type DataSnapshot, get, ref } from "firebase/database";
+import { type DataSnapshot, get, ref, remove } from "firebase/database";
 import { f_db } from "~/server/db_schema";
-import { fb_location_entries, type Location } from "~/server/db_schema/fb_schema";
+import { type Technician, fb_location_entries, type Location, fb_location_roster } from "~/server/db_schema/fb_schema";
+import { assign_technician_to_roster, retrieve_roster_ids } from "../crud/location/location_roster";
+import { get_active_technicians, mark_technician_active } from "./technician_queries";
 
 export async function get_location_list(redirect = ""): Promise<Location[]> {
     const data: DataSnapshot = await get(ref(f_db, fb_location_entries(redirect)));
@@ -13,4 +15,25 @@ export async function get_location_list(redirect = ""): Promise<Location[]> {
     }
 
     return locations;
+}
+
+export async function assign_technician_to_location({ location_id, technician }: { location_id: string, technician: Technician}, redirect = "") {
+    await assign_technician_to_roster({ 
+        location_id: location_id, 
+        technician:{ id: technician.id, color: technician.color } 
+    }, redirect);
+
+    await mark_technician_active(technician, redirect);
+}
+
+export async function remove_technician_from_location({ location_id, technician_id }: { location_id: string, technician_id: string}, redirect = "") {
+    await remove(ref(f_db, fb_location_roster(redirect).concat(location_id, "/", technician_id)));
+}
+
+export async function retrieve_technicians_at(location_id: string, redirect = ""): Promise<Technician[]> {
+    const active_tech: Technician[] = await get_active_technicians(redirect);
+    const roster: string[] = await retrieve_roster_ids(location_id, redirect);
+    console.log(roster)
+    console.log(active_tech)
+    return active_tech.filter((tech) => (roster.includes(tech.id)));
 }
