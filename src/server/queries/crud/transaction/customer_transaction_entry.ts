6c15e8198b5c07d31e2360db880e565c65cba92a
xXAnoHitoXx <AnoHito.TurtleD@gmail.com>
2024-09-10 12:27:49 -0300

    another hot fix

--- src/server/queries/crud/transaction/customer_transaction_entry.ts
+++ src/server/queries/crud/transaction/customer_transaction_entry.ts
@@ -10,20 +10,20 @@ import { get, remove, set } from "firebase/database";
 import { is_string } from "~/server/validation/simple_type";
 
 export const create_customer_trasaction_history_entry: Query<
-    { customer_id: string; id: string; date: string },
+    { customer_id: string; id: string; date: string; salon: string },
     void
-> = async ({ customer_id, id, date }, f_db) => {
+> = async ({ customer_id, id, date, salon }, f_db) => {
     const context = "Creating Customer Transaction History entry";
 
     const ref = f_db.customers_transaction_history([customer_id, id]);
 
-    const e = await db_query(context, set(ref, date));
+    const e = await db_query(context, set(ref, { date: date, salon: salon }));
     if (is_data_error(e)) return e;
 };
 
 export const retrieve_customer_transactions_history: Query<
     { customer_id: string },
-    PartialResult<{ id: string; date: string }[]>
+    PartialResult<{ id: string; date: string; salon: string }[]>
 > = async ({ customer_id }, f_db) => {
     const context = "Retrieving transaction history of ".concat(customer_id);
 
@@ -35,13 +35,13 @@ export const retrieve_customer_transactions_history: Query<
         return { data: [], error: null };
     }
 
-    const transactions: { id: string; date: string }[] = [];
+    const transactions: { id: string; date: string; salon: string }[] = [];
     const error: DataError[] = [];
 
     data.forEach((child) => {
-        const date = child.val();
-        if (is_string(date)) {
-            transactions.push({ id: child.key, date: date });
+        const { date, salon } = child.val();
+        if (is_string(date) && is_string(salon)) {
+            transactions.push({ id: child.key, date: date, salon: salon });
         } else {
             error.push(
                 data_error(context, child.key.concat(" value is not a string")),
