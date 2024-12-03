45d41dc4d54b4ee7297c5393dba32cb9db4eafde
ano <anohito.turtled@gmail.com>
2024-12-03 13:04:04 -0400

    DateRangePicker

--- src/app/salon/app-view/_activity/sumary_view.tsx
+++ src/app/salon/app-view/_activity/sumary_view.tsx
@@ -1,3 +1,19 @@
+import { CalendarDate, RangeValue } from "@nextui-org/react";
+import { useState } from "react";
+import { BoardDateRangePicker } from "../_components/date_range_picker";
+import { last_sunday } from "~/server/validation/semantic/date";
+
 export function SummaryView() {
-    return <div>Not Implemented</div>;
+    const [date_range, set_date_range] = useState<RangeValue<CalendarDate>>({
+        start: last_sunday().subtract({ weeks: 1 }),
+        end: last_sunday(),
+    });
+    return (
+        <div>
+            <BoardDateRangePicker
+                dates={date_range}
+                set_date={set_date_range}
+            />
+        </div>
+    );
 }
---
+++ src/app/salon/app-view/_components/date_range_picker.tsx
@@ -0,0 +1,68 @@
+import { CalendarDate, DateValue } from "@internationalized/date";
+import {
+    Button,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+    RangeCalendar,
+    RangeValue,
+} from "@nextui-org/react";
+import { Dispatch, SetStateAction, useState } from "react";
+
+export function BoardDateRangePicker(props: {
+    dates: RangeValue<CalendarDate>;
+    set_date: Dispatch<SetStateAction<RangeValue<CalendarDate>>>;
+}) {
+    const [holder, update_date] = useState<RangeValue<DateValue>>({
+        start: props.dates.start,
+        end: props.dates.end,
+    });
+
+    return (
+        <div className="flex w-3/4 flex-wrap gap-1">
+            <Popover
+                placement="bottom"
+                onClose={() => {
+                    props.set_date({
+                        start: new CalendarDate(
+                            holder.start.year,
+                            holder.start.month,
+                            holder.start.day,
+                        ),
+                        end: new CalendarDate(
+                            holder.end.year,
+                            holder.end.month,
+                            holder.end.day,
+                        ),
+                    });
+                }}
+            >
+                <PopoverTrigger>
+                    <Button className="w-64">
+                        {holder.start.toString() +
+                            " <-> " +
+                            holder.end.toString()}
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                    <RangeCalendar
+                        classNames={{
+                            base: "h-96 w-fit",
+                            content: "h-96 w-fit",
+                            gridHeaderCell: "w-12 h-12",
+                            cell: "w-12 h-12",
+                            cellButton: "w-12 h-12",
+                        }}
+                        visibleMonths={
+                            holder.start.month + 1 === holder.end.month ? 2 : 1
+                        }
+                        hideDisabledDates={true}
+                        weekdayStyle="short"
+                        value={holder}
+                        onChange={update_date}
+                    />
+                </PopoverContent>
+            </Popover>
+        </div>
+    );
+}
