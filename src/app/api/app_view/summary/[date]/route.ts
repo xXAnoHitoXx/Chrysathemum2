d1cddb220dcebec284a200411955c784e6120ba0
ano <anohito.turtled@gmail.com>
2025-01-11 16:42:21 -0400

    route protection

--- src/app/api/app_view/summary/[date]/route.ts
+++ src/app/api/app_view/summary/[date]/route.ts
@@ -1,7 +1,10 @@
+import { require_permission, Role } from "~/app/api/c_user";
 import { unpack_response } from "~/app/api/server_parser";
 import { handle_partial_errors } from "~/server/data_error";
 import { get_all_technicians } from "~/server/queries/business/technician/technician_queries";
-import { get_bisquit } from "~/server/queries/crud/biscuits"; import { retrieve_earnings_information_of_date } from "~/server/queries/earnings/mod"; import { TechAccount } from "~/server/queries/earnings/types";
+import { get_bisquit } from "~/server/queries/crud/biscuits";
+import { retrieve_earnings_information_of_date } from "~/server/queries/earnings/mod";
+import { TechAccount } from "~/server/queries/earnings/types";
 import { pack, pack_nested } from "~/server/queries/server_queries_monad";
 import { Bisquit } from "~/server/validation/bisquit";
 
@@ -9,6 +12,10 @@ export async function GET(
     _: Request,
     { params }: { params: Promise<{ date: string }> },
 ) {
+    await require_permission([Role.Operator, Role.Admin]).catch(() => {
+        return Response.error();
+    });
+
     const { date } = await params;
 
     const query = pack(Bisquit.salon_selection)
@@ -33,9 +40,10 @@ export async function GET(
                             }
                         }
                     }
-                    
+
                     return data;
-                }).unpack();
+                })
+                .unpack();
         });
 
     return unpack_response(query);
