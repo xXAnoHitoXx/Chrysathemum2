9237c24731c2b5d632fb3708b2cbda7eb1468fff
ano <anohito.turtled@gmail.com>
2025-02-08 11:06:47 -0400

    negative money

--- src/util/money.ts
+++ src/util/money.ts
@@ -1,6 +1,13 @@
 export function money(m: number) {
+    let prefix = "$";
+
+    if (m < 0) {
+        prefix = "-$";
+        m = Math.abs(m);
+    }
+
     const cents = m % 100;
     const dollars = Math.floor(m / 100);
     const cents_string: string = cents < 10 ? "0" + cents : cents.toString();
-    return "$" + dollars + "." + cents_string;
+    return prefix + dollars + "." + cents_string;
 }
