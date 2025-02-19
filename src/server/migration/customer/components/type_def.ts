9f7f294416e902421875b453599131a3c08293b7
ano <anohito.turtled@gmail.com>
2025-02-19 17:24:34 -0400

    discount fix

--- src/server/migration/customer/components/type_def.ts
+++ src/server/migration/customer/components/type_def.ts
@@ -6,3 +6,9 @@ export const OldCustomerEntry = z.object({
     phoneNumber: z.number(),
 });
 export type OldCustomerEntry = z.infer<typeof OldCustomerEntry>;
+
+export const LegacyCustomerIndex = z.object({
+    legacy_id: z.string(),
+    customer_id: z.string(),
+}) 
+export type LegacyCustomerIndex = z.infer<typeof LegacyCustomerIndex>;
